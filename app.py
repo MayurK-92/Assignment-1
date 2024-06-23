5a02ba40d29f9c373acbccc5d96f3654a41d5c55
username <useremail>
2024-06-23 15:17:29 +0530

    Added new endpoint /new-feature

--- app.py
+++ app.py
@@ -9,3 +9,13 @@ def read_root():
 @app.get("/about")
 def about():
     return {"msg": "About Us"}
+
+@app.get("/courses")
+def get_courses():
+    courses = [
+        {"id": 1, "name": "DevOps", "instructor": "Dr.Jyotika Chheda"},
+        {"id": 2, "name": "Internet of Things", "instructor": "Dr.Hiren Dand"},
+        {"id": 3, "name": "Big Data and NOSQL ", "instructor": "Dr.Vishal Borude"},
+        {"id": 4, "name": "Artificial Intelligence", "instructor": "Dr.Priti Pathak"}
+    ]
+    return {"courses": courses}
